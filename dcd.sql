db01a91765bea28dc7620631242106cf35dc79a4
AMIT ANAND <amitanand.kvj.2@gmail.com>
2019-03-08 12:22:36 +0530

    df

---
+++ dcd.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(30) NOT NULL,
+  `lname` varchar(30) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
